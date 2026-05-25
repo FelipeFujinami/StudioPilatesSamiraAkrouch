0721478b974fafb05a401bc18f9f4017225ec07d
FelipeFujinami <gakusei_fujinami03@outlook.com>
2026-05-24 22:28:32 -0300

    Ajustando chaves de email

--- Site/core/settings.py
+++ Site/core/settings.py
@@ -127,4 +127,9 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 AUTH_USER_MODEL = 'studio.CustomUser'
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
